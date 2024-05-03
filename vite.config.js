b9972d0879bce8dd4c83a8d9a63287ac628a40e7
Samuel Cristobal <el.tobal@gmail.com>
2024-05-03 20:12:40 +0200

    Fix MPA build

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 import * as child from 'child_process';
 
@@ -7,6 +8,15 @@ const commitHash = child.execSync('git rev-parse --short HEAD').toString();
 // https://vitejs.dev/config/
 export default defineConfig({
     define: { 'import.meta.env.VITE_APP_VERSION': JSON.stringify(commitHash) },
-    build: { target: 'esnext' },
+    build: {
+        target: 'esnext',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                gameOfLife: resolve(__dirname, 'game-of-life/index.html'),
+                renderer: resolve(__dirname, 'renderer/index.html')
+            }
+        }
+    },
     appType: 'mpa'
 });
